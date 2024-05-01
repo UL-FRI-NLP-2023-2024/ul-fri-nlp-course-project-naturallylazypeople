390ddbbce257d8877fb0ec56e6b05925b7921249
Andrej Susnik <susnik.andrej7@gmail.com>
2024-05-01 14:37:29 +0200

    Disable ssl check in slo super glue

--- src/dataset_handler/slo_super_glue.py
+++ src/dataset_handler/slo_super_glue.py
@@ -9,6 +9,14 @@ import nltk
 from nltk.corpus import stopwords
 
 # Download NLTK resources if not already downloaded
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
 nltk.download('punkt')
 nltk.download('stopwords')
 
